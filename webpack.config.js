6efc122e01f9a2f05c081b41bf090ef2a6408a32
Glynn Phillips <glynndominicphillips@gmail.com>
2018-08-24 12:52:34 +0100

    Sacking off the site generators
    Its just simpler for a site of this size to hardcode stuff.

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: "./src/main.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  plugins: [
+    new MiniCssExtractPlugin({})
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          "css-loader",
+          "sass-loader"
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [{
+          loader: "file-loader",
+          options: {
+            outputPath: 'fonts'
+          }
+        }]
+      }
+    ]
+  }
+};
