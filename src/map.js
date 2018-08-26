b2aa858b5d60b7ef8e929edfc0cc7b6c7e23f1ad
Glynn Phillips <glynndominicphillips@gmail.com>
2018-08-26 20:14:17 +0100

    Testing mapbox.js rather than mapbox gl

--- src/map.js
+++ src/map.js
@@ -1,3 +1,4 @@
+/*
 mapboxgl.accessToken = 'pk.eyJ1IjoiZ2x5bm5waGlsbGlwcyIsImEiOiJjams2eXhkZmUweXBkM3FrM3Vwb3phcmxqIn0.2hTFJAe4vzRLTsvkVhq7HQ';
 
 const map = new mapboxgl.Map({
@@ -8,3 +9,15 @@ const map = new mapboxgl.Map({
 });
 
 map.scrollZoom.disable();
+*/
+
+L.mapbox.accessToken = 'pk.eyJ1IjoiZ2x5bm5waGlsbGlwcyIsImEiOiJjams2eXhkZmUweXBkM3FrM3Vwb3phcmxqIn0.2hTFJAe4vzRLTsvkVhq7HQ';
+
+var map = L.mapbox.map('map', 'mapbox.streets')
+    .setView([50.388866,-5.017080], 13);
+
+var styleLayer = L.mapbox.styleLayer('mapbox://styles/glynnphillips/cjk795lf376tg2rscoi5jozg8')
+    .addTo(map);
+
+map.dragging.disable()
+map.scrollWheelZoom.disable()
