8331bac5555f5947fe8045034f06105049cdef1a
Glynn Phillips <glynndominicphillips@gmail.com>
2018-08-27 12:12:14 +0100

    Adds vague venue marker

--- src/map.js
+++ src/map.js
@@ -14,10 +14,17 @@ map.scrollZoom.disable();
 L.mapbox.accessToken = 'pk.eyJ1IjoiZ2x5bm5waGlsbGlwcyIsImEiOiJjams2eXhkZmUweXBkM3FrM3Vwb3phcmxqIn0.2hTFJAe4vzRLTsvkVhq7HQ';
 
 var map = L.mapbox.map('map', 'mapbox.streets')
-    .setView([50.388866,-5.017080], 13);
+  .setView([50.378687,-5.001103], 12);
 
 var styleLayer = L.mapbox.styleLayer('mapbox://styles/glynnphillips/cjk795lf376tg2rscoi5jozg8')
     .addTo(map);
+
+L.circle([50.378687,-5.001103], 2000, {
+	color: '#004c53',
+	fill: '#004c53'
+}).addTo(map);
+
+
 
 map.dragging.disable()
 map.scrollWheelZoom.disable()
