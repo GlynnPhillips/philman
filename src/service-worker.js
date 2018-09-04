fe3425beb8681fe951514d56a9821bdb6cd45bf7
Glynn Phillips <glynndominicphillips@gmail.com>
2018-09-04 22:22:53 +0100

    Adds main.css and index.html to the cache
    Moving more files to the cache so hopefully we can serve the page whilst
    offline.

--- src/service-worker.js
+++ src/service-worker.js
@@ -2,12 +2,15 @@ self.addEventListener('install', event => {
 	event.waitUntil(
 		caches.open('wasson').then(cache => {
 			return cache.addAll([
+				'/',
+				'index.html',
 				'/dist/fonts/big-john.ttf',
 				'/dist/fonts/big-john.woff',
 				'/dist/fonts/big-john.woff2',
 				'/dist/fonts/Champagne-&-Limousines.ttf',
 				'/dist/fonts/Champagne-&-Limousines.woff',
-				'/dist/fonts/Champagne-&-Limousines.woff2'
+				'/dist/fonts/Champagne-&-Limousines.woff2',
+				'/dist/main.css'
 			]);
 		})
 	);
@@ -17,7 +20,6 @@ self.addEventListener('fetch', event => {
 	if (event.request.url.startsWith(self.location.origin) && event.request.method === 'GET') {
 		event.respondWith(
 			caches.match(event.request).then(cachedResponse => {
-				console.log('Cache response', cachedResponse)
 				return cachedResponse || fetch(event.request);
 			})
 		);
